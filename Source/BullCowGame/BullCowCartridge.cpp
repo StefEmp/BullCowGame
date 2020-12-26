1a38c94e306a23d95a21cc26638c03a7ba7ad92d
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-26 21:39:17 +0000

    Implemented a welcome message on in-game terminal via cartridge and started to use FString

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 #include "BullCowCartridge.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to Bull Cows!");
+    PrintLine("Type something!");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = "About";
 }
