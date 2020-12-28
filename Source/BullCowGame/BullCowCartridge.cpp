e31b2c5723318b2623d85bf8f725248e81cc0484
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-28 16:54:25 +0000

    Added comments as pseudo code to create a framework for moving forward

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,16 +4,25 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    //welcome player
     PrintLine (TEXT("Welcome to Bull Cows!"));
     PrintLine (TEXT("Guess the 4 letter word!")); // we have a magic number will need to create a variable to manage number difference later
     PrintLine (TEXT("Press enter to continue..."));
-    HiddenWord = TEXT("cake");
+
+    //set up game
+    HiddenWord = TEXT("cake"); // set the HiddenWord
+
+    //set lives
+
+    //prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
     
+    //checking PlayerGuess
+
     if ( Input == HiddenWord) 
     {
        PrintLine (TEXT("You have won!"));
@@ -23,7 +32,20 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     {
         {
             PrintLine (TEXT("You have lost!"));
+
+            
         }
     }
-    
+    //Check if Isogram
+    //prompt to guess again
+    //check if right number of characters
+    //prompt to guess again
+    //remove a life
+    //check if lives > 0
+    // if yes GuessAgain
+    //Show Lives left
+    //if no show GameOver and HiddenWord?
+    //press enter to play again?
+    //check user input
+    //PlayAgain or Quit
 }
