22aedca69daf34a2b763af67fe937b4735ce0971
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-28 16:45:06 +0000

    Added a refined game loop flowchart for refined plan for the game in documentation folder

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -7,12 +7,12 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine (TEXT("Welcome to Bull Cows!"));
     PrintLine (TEXT("Guess the 4 letter word!")); // we have a magic number will need to create a variable to manage number difference later
     PrintLine (TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake"); // Move outside of this function so it's scope is outside this function
     
     if ( Input == HiddenWord) 
     {
