61643cb01a934c1a7fa2b5c11d58e77029e830d0
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-28 16:21:28 +0000

    declared and initialised HiddenWord in header file to fix it's scope so we can assign hiddenword to different strings

--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -14,8 +14,8 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
-
+	
 	// Your declarations go below!
 	private:
-	
+	FString HiddenWord;
 };
