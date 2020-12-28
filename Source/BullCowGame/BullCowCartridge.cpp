d14c2344c101ab8a61ad2a29a83d81a92c63fec6
StefEmp <stephane.emptage@hotmail.co.uk>
2020-12-28 16:13:32 +0000

    Added a win/lose if statement, while it works will need to refactor later due to scoping

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,25 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine (TEXT("Welcome to Bull Cows!"));
-    PrintLine (TEXT("Type something!"));
+    PrintLine (TEXT("Guess the 4 letter word!")); // we have a magic number will need to create a variable to manage number difference later
+    PrintLine (TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("About");
-    PrintLine(Input);
+    FString HiddenWord = TEXT("cake"); // Move outside of this function so it's scope is outside this function
+    
+    if ( Input == HiddenWord) 
+    {
+       PrintLine (TEXT("You have won!"));
+    }
+
+    else
+    {
+        {
+            PrintLine (TEXT("You have lost!"));
+        }
+    }
+    
 }
